d6e58a03d72e9f5cfa1b4eac77c90e3eafb24302
ljiljanaknezevic <58477796+ljiljanaknezevic@users.noreply.github.com>
2021-05-12 10:54:36 +0200

    User logout,change password

--- spring-security-example/src/main/resources/static/scripts/patient.js
+++ spring-security-example/src/main/resources/static/scripts/patient.js
@@ -1,8 +1,71 @@
 $(document).ready(function() {
-		$('#edit-profile').attr('hidden', true);
+
 		$('a#logout').click(function(){
 		localStorage.removeItem('jwt')		
 		location.href = "login.html";
+		});
+		
+			$('a#changePassword').click(function(){	
+		$('#show').attr('hidden',false);
+		input_password = $('#id_password');
+		console.log(input_password)
+		input_passwordConf = $('#id_passwordConf');
+  		var btnChange = document.getElementById("btnChange")
+		console.log(btnChange)
+  		btnChange.disabled = true
+  
+  input_password.keyup(function () {
+	  	if(!validatePassword(input_password.val())){
+	  		btnChange.disabled = true
+			$(this).addClass(`alert-danger`);
+	  		$('#id_password').css('border-color', 'red');
+	  		$("#errorPassword").text("Password must have at least 8 characters, lower case, upper case, digit, special character!")
+	  		$('#errorPassword').css('color', 'red');
+	  	}else {
+	  		$(this).removeClass(`alert-danger`);
+	  		$('#id_password').css('border-color', '');
+	  		$("#errorPassword").text("")
+	  	}
+  });	
+	input_passwordConf.keyup(function () {
+	  	if(input_password.val()!=input_passwordConf.val()){
+	  		btnChange.disabled = true
+			$(this).addClass(`alert-danger`);
+	  		$('#id_passwordConf').css('border-color', 'red');
+	  		$("#errorPasswordConf").text("Passwords must match!")
+	  		$('#errorPasswordConf').css('color', 'red');
+	  	}else {
+		
+	  		$(this).removeClass(`alert-danger`);
+	  		$('#id_passwordConf').css('border-color', '');
+	  		$("#errorPasswordConf").text("")
+			btnChange.disabled = false;
+	  	}
+  });
+	$('#btnChange').click(function() {
+		var newPassword = $('#id_password').val()
+		var confirmPassword = $('#id_passwordConf').val()
+		var decoded = parseJwt(localStorage.getItem('jwt'));
+		var email = decoded.email
+		obj = JSON.stringify({email:email,newPass:newPassword,confirmPass:confirmPassword});
+		customAjax({
+	        method:'POST',
+	        url:'/auth/changePassword',
+	        data : obj,
+	        contentType: 'application/json',
+	        success: function(){
+				localStorage.removeItem('email');
+	        	alert("Success changed password!")
+				location.href = "patient.html";
+			},
+			error: function(){
+				localStorage.removeItem('email');
+				alert("User with that email doesn't exist")
+			}
+	            });
+
+		});
+	
 		});
 		
 	$('a#profile').click(function(){
