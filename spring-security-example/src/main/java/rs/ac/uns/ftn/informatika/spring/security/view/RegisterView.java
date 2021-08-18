72db96bbae1bf9c306178ac7e76d4226a365a7ff
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2021-08-18 12:11:41 +0200

    Register Dermatologist Front

---
+++ spring-security-example/src/main/java/rs/ac/uns/ftn/informatika/spring/security/view/RegisterView.java
@@ -0,0 +1,106 @@
+package rs.ac.uns.ftn.informatika.spring.security.view;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Column;
+import java.sql.Timestamp;
+
+public class RegisterView {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String country;
+    private String city;
+    private String address;
+    private String phoneNumber;
+
+    public RegisterView() {
+    }
+
+    public RegisterView(String username, String password, String firstName, String lastName, String email, String country, String city, String address, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
